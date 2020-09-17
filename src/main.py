c58cddf5da8cf1dee3714ee6fc3f46e15e13f7c1
rish-16 <mail.rishabh.anand@gmail.com>
2020-09-17 10:03:10 +0800

    Add ResNet50; Run SAA on model

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
 import numpy as np
 import tensorflow as tf
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input, decode_predictions
 from point import Point
+from pprint import pprint
+import matplotlib.pyplot as plt
 
 class SimulatedAnnealingAttack:
     def __init__(self, model, image, true_label, target_label):
@@ -24,3 +28,16 @@ class SimulatedAnnealingAttack:
         shape = self.image.shape
         
         random_point = Point()
+
+model = ResNet50(include_top=True, weights="imagenet")
+
+img_path = "../assets/elephant.jpg"
+img = image.load_img(img_path, target_size=(224, 224)) # standard resnet input dims
+img = image.img_to_array(img)
+img = np.expand_dims(img, axis=0)
+img = preprocess_input(img)
+
+preds = model.predict(img)
+label = decode_predictions(preds, top=1)
+
+sim_attack = SimulatedAnnealingAttack(model, img, label, 34)
