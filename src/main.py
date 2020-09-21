7f041c8811841e7475ae631d2951725412b2249d
rish-16 <mail.rishabh.anand@gmail.com>
2020-09-22 01:14:01 +0800

    Add image processing methods

--- src/main.py
+++ src/main.py
@@ -11,21 +11,15 @@ model = ResNet50(include_top=True, weights="imagenet")
 img_path = "../assets/dog2.jpg"
 img = image.load_img(img_path, target_size=(224, 224)) # standard resnet input dims
 img = image.img_to_array(img) / 255
-x = np.expand_dims(img, axis=0)
-x = preprocess_input(x)
-
-preds = model.predict(x)
-labels = decode_predictions(preds, top=1)
-print (labels)
 
 T_max = 1000
 T_min = 1
 T_delta = 0.01
-temps = np.arange(T_max, T_min, T_delta)
+temps = np.arange(T_max, T_min, -T_delta)
 
 all_images = []
 all_epsilons = []
-all_images.append(x)
+all_images.append(img)
 
 def mutate_point(pt):
     random_color = [np.random.uniform(0, 1), np.random.uniform(0, 1), np.random.uniform(0, 1)] # RGB Channels
@@ -33,42 +27,49 @@ def mutate_point(pt):
     
     return pt
     
+def get_confidence(img):
+    x = np.expand_dims(img, axis=0)
+    x = preprocess_input(img)
+    pred = model.predict(x)
+    label = decode_predictions(pred, top=1)
+    confidence = label[0][0][-1]
+    
+    return confidence
+    
+def superimpose(pt, img):
+    img_copy = img.copy()
+    img_copy[pt.coords['x']][pt.coords['y']] = pt.colour
+    
+    return img_copy
+    
+print ("Poisoning image...")
 for i in range(len(temps)):
     T = temps[i]
     
-    point = Point(x)
+    point = Point(img)
     
-    img_point = point.superimpose(x)
+    img_point = superimpose(point, img)
     
-    epsilon = decode_predictions(model.predict(x), top=1)[0][0][-1]
-    epsilon_point = decode_predictions(model.predict(img_point), top=1)[0][0][-1]
+    epsilon = get_confidence(img)
+    epsilon_point = get_confidence(img_point)
     
     if epsilon_point < epsilon: # new image lowers confidence more
         mut_point = mutate_point(point)
-        img_mut_point = mut_point.superimpose(x)
-        epsilon_mut = decode_predictions(model.predict(img_mut_point), top=1)[0][0][-1]
+        img_mut_point = superimpose(mut_point, img)
+        epsilon_mut = get_confidence(img_mut_point)
         
         if epsilon_mut < epsilon_point:
-            x = img_mut_point
+            img = img_mut_point
             epsilon = epsilon_mut
         else:
-            x = img_point
+            img = img_point
             epsilon = epsilon_point
         
     elif np.exp(-epsilon / T) < np.random.uniform(0, 1): # not using Boltzmann Constant k
-        x = img_point
+        img = img_point
         
     if i % 50 == 0:
         print ("Current iteration: {} | Temperate: {} | Epsilon: {}".format(i+1, T, epsilon))
         
-    all_images.append(x)
+    all_images.append(img)
     all_epsilons.append(epsilon)
-    
-plt.tight_layout()
-plt.figure(1)
-plt.subplot(121)
-plt.imshow(all_images[0])
-plt.axis("off")
-plt.subplot(122)
-plt.imshow(all_images[-1])
-plt.axis("off")
--- src/point.py
+++ src/point.py
@@ -7,14 +7,7 @@ class Point:
         self.colour = [np.random.uniform(0, 1), np.random.uniform(0, 1), np.random.uniform(0, 1)] # RGB Channels
         self.coords = {}
         
-        x, y = np.where(og_img <= 1)
-        random_idx = np.random.randint(len(x))
+        random_idx = np.random.randint(len(og_img))
         
-        self.coords['x'] = x[random_idx]
-        self.coords['y'] = y[random_idx]
-        
-    def superimpose(self, img):
-        img_copy = img.copy()
-        img_copy[self.coords['x']][self.coords['y']] = self.colour
-        
-        return img, img_copy
+        self.coords['x'] = random_idx
+        self.coords['y'] = random_idx
