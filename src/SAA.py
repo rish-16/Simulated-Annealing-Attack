44a3dd48fd853d0152932bef747cbb29eed9c429
rish-16 <mail.rishabh.anand@gmail.com>
2020-12-10 23:06:51 +0800

    Add NRMSE to control image volatility

--- src/SAA.py
+++ src/SAA.py
@@ -1,10 +1,11 @@
+import copy
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.keras.models import Sequential, load_model
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.datasets import mnist
-from utils import Point
+from utils import Mask
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train = x_train.reshape([len(x_train), 28, 28, 1]) / 255
@@ -15,37 +16,40 @@ y_test = to_categorical(y_test, 10)
 
 model = load_model("../bin/mnist_cnn.h5")
 
-original = x_test[0]
+original = copy.deepcopy(x_test[0])
 image = x_test[0]
 label = y_test[0].max()
 
-plt.imshow(image.reshape([28, 28]), cmap="gray")
-plt.title(list(range(0, 9))[y_train.argmax()])
-plt.show()
+# plt.imshow(original.reshape([28, 28]), cmap="gray")
+# plt.title(y_test[0].argmax())
+# plt.show()
+
+def normalised_RMSE(img, cpy):
+    mse = np.sum(np.square(img - cpy))
+    return np.sqrt(mse)
 
 all_confidences = []
 
-T_MIN, T_MAX, T_DELTA = 1, 1000, 0.01
+T_MIN, T_MAX, T_DELTA = 1, 1000, 1
 temperatures = np.arange(T_MAX, T_MIN, -T_DELTA)
 
 for i in range(len(temperatures)):
     t = temperatures[i]
-    
-    if i % 25 == 0:
-        print ("Temperature: {}".format(t))
-        
-    point = Point(image)
-    logit = model.predict(image)
+                
+    logit = model.predict(np.array([image]))
     initial_confidence = logit.max()
-    image_with_point = point.superimpose(image)
-    confidence_with_point = model.predict(image_with_point).max()
     
-    if confidence_with_point < initial_confidence:
-        new_point = Point(image)
+    point = Mask()
+    image_with_point = point.superimpose(image)
+    confidence_with_point = model.predict(np.array([image_with_point])).max()
+    print (normalised_RMSE(image, image_with_point), confidence_with_point)
+    
+    if confidence_with_point < initial_confidence and normalised_RMSE(image, image_with_point) < 0.2:
+        new_point = Mask()
         image_with_new_point = new_point.superimpose(image_with_point)
-        confidence_with_new_point = model.predict(image_with_new_point).max()
+        confidence_with_new_point = model.predict(np.array([image_with_new_point])).max()
         
-        if confidence_with_new_point < confidence_with_point:
+        if confidence_with_new_point < confidence_with_point and normalised_RMSE(image_with_point, image_with_new_point) < 0.2:
             image = image_with_new_point
             initial_confidence = confidence_with_new_point
         else:
@@ -58,14 +62,16 @@ for i in range(len(temperatures)):
         
     all_confidences.append(initial_confidence)
         
-plt.figure(1)        
+fig = plt.figure(1)        
 
 plt.subplot(131)
 plt.imshow(original.reshape([28, 28]), cmap="gray")
+plt.title(model.predict(np.array([original])).argmax())
 plt.axis("off")
 
 plt.subplot(132)
 plt.imshow(image.reshape([28, 28]), cmap="gray")
+plt.title(model.predict(np.array([image])).argmax())
 plt.axis("off")
 
 plt.subplot(133)
--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,14 @@
 import numpy as np
 
-class Point:
-    def __init__(self, image):
-        self.image = image
+class Mask:
+    def superimpose(self, image):
+        w  = image.shape[0]
+        h  = image.shape[1]
         
-    def gen_point(self):
-        pass
-    
-    def superimpose(self):
-        pass
+        r_loc = np.random.randint(0, h)
+        c_loc = np.random.randint(0, w)
+        
+        color = np.random.randint(0, 255) / 255
+        image[r_loc][c_loc] = color
+        
+        return image
